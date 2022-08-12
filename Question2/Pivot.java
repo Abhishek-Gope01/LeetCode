9d66bc44d603bfc0d4036ab41b47b3f637570644
Abhishek-Gope01 <abhishekgope2244@gmail.com>
2022-08-12 22:02:50 +0530

    Dont need this

--- Question2/Pivot.java
+++
@@ -1,8 +0,0 @@
-public class Pivot {
-    public static void hello(String str){
-        System.out.println(str);
-    }
-    public static void main(String[] args) {
-        hello("hello");
-    }
-}
